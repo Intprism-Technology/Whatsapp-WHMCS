e010590690a71aaee8ae4d3990fc57201a16b669
Muhammad Habib Ulil A <intprismtechnology@gmail.com>
2023-03-03 13:23:34 +0700

    Update invoice_comingTerminate.py

--- whmcs/invoice_comingTerminate.py
+++ whmcs/invoice_comingTerminate.py
@@ -1,18 +1,19 @@
 import config
-import datetime
 import requests
 import template_message
-
-currentDate = datetime.datetime.now()
-currentDate = currentDate.year
+from datetime import date
+from datetime import timedelta
 
 access = config.db.cursor()
 access.execute("SELECT value FROM tblconfiguration WHERE setting = 'AutoTerminationDays';")
 terminateConf = access.fetchall()
 dayBeforeTerminate = 1
 
+today = date.today()
+terminateDate = [today - timedelta(days = int(terminateConf[0][0])-int(dayBeforeTerminate))]
+
 access = config.db.cursor()
-access.execute("SELECT * FROM tblinvoices WHERE status = 'Unpaid' AND duedate = CURDATE()-{};".format(int(terminateConf[0][0])-int(dayBeforeTerminate)))
+access.execute("SELECT * FROM tblinvoices WHERE status = 'Unpaid' AND duedate = %s;",terminateDate)
 resultInvoice = access.fetchall()
 for x in resultInvoice:
     sql = "SELECT * FROM tblclients WHERE id = %s"
