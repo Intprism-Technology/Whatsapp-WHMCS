d142255ec2cd5838865d8760912c71ce91f785c9
Muhammad Habib Ulil A <intprismtechnology@gmail.com>
2023-08-06 17:34:31 +0700

    Update ticket.py

--- whmcs/ticket.py
+++ whmcs/ticket.py
@@ -26,21 +26,21 @@ for x in resultNewTicket:
 
 # Reply ticket
 access = config.db.cursor()
-access.execute("SELECT * FROM tblticketreplies WHERE date >= date_sub(now(), interval 5 minute); ")
+access.execute("SELECT * FROM `tblticketreplies` WHERE admin != "" AND date >= date_sub(now(), interval 5 minute)")
 resultReplyTicket = access.fetchall()
 for x in resultReplyTicket:
+    sql = "SELECT * FROM tbltickets WHERE id = %s"
+    access.execute(sql, (x[1],))
+    getTicket = access.fetchall()
     sql = "SELECT * FROM tblclients WHERE id = %s"
-    access.execute(sql, (x[2],))
+    access.execute(sql, (getTicket[0][2],))
     resultUser = access.fetchall()
     for user in resultUser:
         firstName = user[2]
         lastName = user[3]
         phone = user[12].replace('.', '').replace('-', '')
         #get ticket id
-        sql = "SELECT * FROM tbltickets WHERE id = %s"
-        access.execute(sql, (x[1],))
-        ticketID = access.fetchall()
-        messageToSend = template_message.reply_ticket.format(firstName = firstName,lastName = lastName,phone = phone, ticketID = ticketID[0][1], ticketTitle = ticketID[0][12])
+        messageToSend = template_message.reply_ticket.format(firstName = firstName,lastName = lastName,phone = phone, ticketID = getTicket[0][1], ticketTitle = getTicket[0][12])
         url = 'http://127.0.0.1:8080/api/send'
         data = {'phone': phone, 'message': messageToSend}
         sendMessage = requests.post(url, json = data)
